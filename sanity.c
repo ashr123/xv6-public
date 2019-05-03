4b0c0cc82c40d785606be12b0603a53234917ccd
Roy Ash <roykid123@gmail.com>
2019-05-03 16:31:28 +0300

    sdf

--- sanity.c
+++ sanity.c
@@ -1,4 +1,3 @@
-האחסון שלי
 // Test that fork fails gracefully.
 // Tiny executable so that the limit can be filling the proc table.
 
@@ -18,8 +17,15 @@
 
 int counter = 0;
 int mid;
-int help;
+int curr_id;
 trnmnt_tree *tree;
+
+
+void panic(const char str[])
+{
+	printf(2, str);
+	exit();
+}
 
 void
 test2_1(void)
@@ -40,7 +46,7 @@ test2_1(void)
 		exit();
 	} else
 	{
-		printf(1, "fork failed\n");
+		panic("test2_1: fork failed\n");
 	}
 }
 
@@ -54,7 +60,6 @@ thread_task22()
 	kthread_exit();
 }
 
-
 void
 test2_2(void)
 {
@@ -62,16 +67,17 @@ test2_2(void)
 	int threads[8];
 	for (int i = 0; i < 8; i++)
 	{
-		stack = malloc(MAX_STACK_SIZE);
+		if (!(stack = malloc(MAX_STACK_SIZE)))
+			panic("test2_2: malloc(MAX_STACK_SIZE) failed!");
 		threads[i] = kthread_create(&thread_task22, stack);
 		if (threads[i] < 0)
-			printf(1, "kthread_create failed\n");
+			panic("test2_2: kthread_create failed\n");
 	}
 
 
 	for (int i = 0; i < 8; i++)
 		if (kthread_join(threads[i]) < 0)
-			printf(1, "join failed\n");
+			panic("test2_2: join failed\n");
 
 
 	printf(1, "finish join 8 threads\n");
@@ -83,12 +89,12 @@ thread_task31(void)
 {
 	if (kthread_mutex_lock(mid) < 0)
 	{
-		printf(1, "lock failed!\n");
+		panic("lock failed!\n");
 	}
 
 	counter++;
 	if (kthread_mutex_unlock(mid) < 0)
-		printf(1, "unlock failed! thread 2\n");
+		panic("unlock failed! thread 2\n");
 	kthread_exit();
 }
 
@@ -98,68 +104,69 @@ test3_1(void)
 
 	mid = kthread_mutex_alloc();
 	if (kthread_mutex_lock(mid) < 0)
-		printf(1, "lock failed!\n");
-	printf(1, "lock %d !\n", mid);
+		panic("lock failed!\n");
+	printf(1, "lock %d!\n", mid);
 	void *stack;
 	if ((stack = malloc(MAX_STACK_SIZE)) < 0)
-		printf(1, "malloc(MAX_STACK_SIZE) failed!\n");
+		panic("test3_1: malloc(MAX_STACK_SIZE) failed!\n");
 	int thread = kthread_create(&thread_task31, stack);
 	if (thread == -1)
-		printf(1, "test3_1: kthread_create failed!");
+		panic("test3_1: kthread_create failed!");
 	printf(1, "counter = %d , need to be 0\n", counter);
 	// sleep(1000);
 	if (kthread_mutex_unlock(mid) < 0)
-		printf(1, "unlock failed! main\n");
+		panic("unlock failed! main\n");
 	kthread_join(thread);
 	printf(1, "counter = %d , need to be 1\n", counter);
-	printf(1, "unlock %d !\n", mid);
+	printf(1, "unlock %d!\n", mid);
 
 }
 
 void
 thread_task32(void)
 {
-	if (trnmnt_tree_acquire(tree, kthread_id() - help - 1) < 0)
-		printf(1, "aquire failed\n");
+	if (trnmnt_tree_acquire(tree, kthread_id() - curr_id - 1) < 0)
+		panic("aquire failed\n");
 	counter++;
-	printf(1, "thread %d made counter = %d\n", kthread_id() - help, counter);
-	if (trnmnt_tree_release(tree, kthread_id() - help - 1) < 0)
-		printf(1, "release failed\n");
+	printf(1, "thread %d made counter = %c\n", kthread_id() - curr_id, counter);
+	if (trnmnt_tree_release(tree, kthread_id() - curr_id - 1) < 0)
+		panic("release failed\n");
 	kthread_exit();
-
 }
 
 void
 test3_2(void)
 {
-	tree = trnmnt_tree_alloc(3);
-	help = kthread_id();
-	void *stack;
-	int tids[10];
+	if (!(tree = trnmnt_tree_alloc(3)))
+		panic("test3_2: trnmnt_tree_alloc(3) failed!\n");
+
+	counter='A';
+	curr_id = kthread_id();
+	int tids[8];
 	for (int i = 0; i < 8; i++)
 	{
-		stack = malloc(MAX_STACK_SIZE);
-		tids[i] = kthread_create(&thread_task32, stack);
+		if((tids[i] = kthread_create(&thread_task32, malloc(MAX_STACK_SIZE))) < 0)
+			panic("test3_2: kthread_create failed!\n");
 	}
 	for (int i = 0; i < 8; i++)
 	{
 		kthread_join(tids[i]);
 	}
 	if (trnmnt_tree_dealloc(tree) < 0)
-		printf(1, "dealloc failed\n");
-	printf(1, "joined eveyone and the counter is %d when it should be 8", counter);
+		panic("test3_2: dealloc failed\n");
+	printf(1, "joined eveyone and the counter is %d when it should be 8\n", counter-'A');
 }
 
 int
 main(void)
 {
-	// printf(1,"test 2.1 :\n");  
-	// test2_1();
-	// printf(1,"test 2.2 :\n");  
-	// test2_2();
-	printf(1, "test 3.1 :\n");
+//	printf(1, "test 2.1:\n");
+//	test2_1();
+	printf(1, "test 2.2:\n");
+	test2_2();
+	printf(1, "test 3.1:\n");
 	test3_1();
-	// printf(1,"test 3.2 :\n");  
-	// test3_2();
+	printf(1, "test 3.2:\n");
+	test3_2();
 	exit();
 }
